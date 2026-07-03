ef9d33a68281c13eafc08f0bd74490c91deb3928
dlsnansk <dlsnansk@gmail.com>
2026-07-03 03:53:18 +0300

    Add files via upload

---
+++ stg.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# stg.py
+# Tag generator, like in video games and military themes
+import random as r
+import sys, os
+verb=('Shadow', 'Ghost', 'Void', 'Zero', 'Dark', 'Neon', 'Cyber', 'Pulse', 'Flame', 'Steel', 'Rust', 'Echo', 'Storm', 'Canon', 'Alpha', 'Bravo', 'Charlie', 'Delta', 'Gamma', 'Midnight')
+noun=('Packet', 'Route', 'Node', 'Trace', 'Walker', 'Vector', 'Relay', 'Signal', 'Cipher', 'Drift', 'Chrome', 'Dogma', 'Sphere')
+numbers='0123456789'
+cut='''
+=========================
+'''
+def cl():
+    os.system('cls'if os.name=='nt'else 'clear')
+def hello():
+    cl()
+    print(f'''
+===== W E L C O M E =====
+========== T O ==========
+========= S T G =========
+
+the__serial_tag_generator 
+''')
+    input(f'\nPress ENTER to continue... ')
+    cl()
+hello()
+def main():
+    cl()
+    try:
+        while True:
+            t=0
+            tag=''
+            try:
+                usr_nmb=int(input(f'Enter the NUMBER of the TAGs [1-9999]: '))
+                if usr_nmb<=0 or usr_nmb==9999:
+                    continue
+                else:
+                    pass
+            except ValueError:
+                continue
+            print(cut)
+            while True:
+                nma=r.choice(numbers)
+                nmb=r.choice(numbers)
+                vv=r.choice(verb)
+                nn=r.choice(noun)
+                tag=f'{t+1}. {vv}{nn}_{nma}{nmb}'
+                print(tag)
+                t+=1
+                tag=''
+                if t==usr_nmb:
+                    print(cut)
+                    break
+            usr_cnt=input(f'\n[A]gain / [Q]uit ? ').strip().lower()
+            if usr_cnt=='a':
+                cl()
+                continue
+            else:
+                cl()
+                sys.exit()
+    except KeyboardInterrupt:
+        print(f'\nSTG has been stopped... ')
+        sys.exit()
+    except Exception as e:
+        print(f'\n[ERROR] -> {e} ')
+        sys.exit()
+main()
